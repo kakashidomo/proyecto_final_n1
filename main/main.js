6e398cf71dd2138df116ac5ecb98326e99858d60
Gustavo Lastra <lastraemiliano1@gmail.com>
2024-12-11 13:33:15 -0600

    avances en html, css,corregir eror en divs, inicio js

---
+++ main/main.js
@@ -0,0 +1,13 @@
+
+const monto = document.querySelector('#monto')
+const years = document.querySelector('#years')
+const porcent = document.querySelector('#porcent')
+
+
+
+
+function name(params) {
+    
+}
+
+/* let rs = (monto*porcent) (1+porcent)** / (1+porcent)** - 1 */
